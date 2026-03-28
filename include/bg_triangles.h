045683c1ecc371b783e02b30afd00a96df4d3c3f
Oliver Bell <freshollie@gmail.com>
2026-03-28 15:46:51 +0000

    fix portable builds

--- include/bg_triangles.h
+++ include/bg_triangles.h
@@ -25,7 +25,7 @@ typedef struct {
 
 extern void SA2_LABEL(sub_8006DB4)(u8 bg, TriParam1 *arg1, s32 unused, s32 arg3);
 extern void SA2_LABEL(sub_800724C)(u8 bg, TriParam1 *param1);
-extern void SA2_LABEL(sub_80075D0)(u8 bg, u8 param1, u8 param2, s16 param3, s16 param4, u16 param5);
+extern void SA2_LABEL(sub_80075D0)(u8 bg, int_vcount param1, int_vcount param2, s16 param3, s16 param4, u16 param5);
 extern void SA2_LABEL(sub_8007738)(u8 bg, int_vcount minY, int_vcount maxY, u16 param3, u8 param4, u8 param5, u16 param6, u8 param7,
                                    u8 param8, s16 param9, s16 param10);
 extern void SA2_LABEL(sub_8007858)(u8 param0, int_vcount minY, int_vcount maxY, u16 param3, u16 param4);
